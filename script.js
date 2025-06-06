6bafb4cc19421d31d6dbbee4f7dbfce0d58669c6
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-06 19:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+let currentSound = null;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const soundFile = button.getAttribute('data-sound');
+
+    if (currentSound) {
+      currentSound.pause();
+    }
+
+    currentSound = new Audio(soundFile);
+    currentSound.play();
+  });
+});
+
+stopButton.addEventListener('click', () => {
+  if (currentSound) {
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  }
+});
